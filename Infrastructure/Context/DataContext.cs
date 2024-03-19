19039d03a3e80cc0f372b61a1913194351e2b519
Kajsa <kajsaramstrom@live.se>
2024-03-19 13:48:33 +0100

    Add project files.

---
+++ Infrastructure/Context/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<CourseEntity> Courses { get; set; }
+    public DbSet<SubscribeEntity> Subscribers { get; set; }
+}
